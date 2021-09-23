a0c18f3e4a52f97efbbfd709cca55ac5132b9d0c
anne-marziale <annemarziale@hotmail.com>
2021-09-23 15:52:02 +0200

    create a new repo

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import SocialNetwork from '../components/SocialNetwork';
+import DynamicText from '../components/DynamicText';
+
+const Home = () => {
+    return (
+        <div>
+            <div className="home">
+           <Navigation />
+           <SocialNetwork />
+           <div className="home-main">
+               <div className="main-content">
+                    <h1>Anne Marziale</h1>
+                    <h2><DynamicText /></h2>
+               </div>
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default Home;
