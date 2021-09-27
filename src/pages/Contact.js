22065f415b25494cea8b33ff1df165f4233b5ef6
anne-marziale <annemarziale@hotmail.com>
2021-09-27 13:55:51 +0200

    portfolio done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,44 +1,46 @@
-import React from 'react';
-import Mouse from '../components/Mouse';
-import Navigation from '../components/Navigation';
-import Logo from '../components/Logo';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import SocialNetwork from '../components/SocialNetwork';
-import ButtonsBottom from '../components/ButtonsBottom';
-import ContactForm from '../components/ContactForm';
+import React from "react";
+import Navigation from "../components/Navigation";
+import ButtonsBottom from "../components/ButtonsBottom";
+import ContactForm from "../components/ContactForm";
+import SocialNetwork from "../components/SocialNetwork";
+import Logo from "../components/Logo";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Mouse from "../components/Mouse";
 import { motion } from "framer-motion";
 
 const Contact = () => {
-    const variants = {
-        in: {
-            opacity: 1,
-            x: 0
-        },
-        out : {
-            opacity: 0,
-            x: 300,
-        }
-    }
-    const transition = {
-        ease: [.03, .87, .73, .9],
-        duration : .6,
-    }
+  const pageTransition = {
+    in: {
+      opacity: 1,
+      x: 0,
+    },
+    out: {
+      opacity: 0,
+      x: 300,
+    },
+  };
+
+  const transition = {
+    ease: [.03,.87,.73,.9],
+    duration: .6,
+  }
+
   return (
     <main>
       <Mouse />
       <motion.div
-      className="contact"
-      exit="out"
-      animate="in"
-      initial="out"
-      variants={variants}
-      transition={transition}
+        className="contact"
+        exit="out"
+        animate="in"
+        initial="out"
+        variants={pageTransition}
+        transition={transition}
       >
         <Navigation />
         <Logo />
         <ContactForm />
         <div className="contact-infos">
-          <div className="adress">
+          <div className="address">
             <div className="content">
               <h4>adresse</h4>
               <p>121 rue Villebois Mareuil</p>
@@ -50,10 +52,10 @@ const Contact = () => {
               <h4>téléphone</h4>
               <CopyToClipboard text="0662960130" className="hover">
                 <p
-                  style={{ cursor: 'pointer' }}
+                  style={{cursor: 'pointer'}}
                   className="clipboard"
                   onClick={() => {
-                    alert('Téléphone copié !');
+                    alert("Téléphone copié !");
                   }}
                 >
                   06 62 96 01 30
@@ -64,15 +66,12 @@ const Contact = () => {
           <div className="email">
             <div className="content">
               <h4>email</h4>
-              <CopyToClipboard
-                text="annemarziale@hotmail.com"
-                className="hover"
-              >
+              <CopyToClipboard text="annemarziale@hotmail.com" className="hover">
                 <p
-                  style={{ cursor: 'pointer' }}
+                  style={{cursor: 'pointer'}}
                   className="clipboard"
                   onClick={() => {
-                    alert('Email copié !');
+                    alert("Email copié !");
                   }}
                 >
                   annemarziale@hotmail.com
@@ -82,10 +81,10 @@ const Contact = () => {
           </div>
           <SocialNetwork />
           <div className="credits">
-            <p>fromScratch - 2020</p>
+            <p>fromAnne - 2021</p>
           </div>
         </div>
-        <ButtonsBottom left={'/project-4'} />
+        <ButtonsBottom left={"/projet-4"} />
       </motion.div>
     </main>
   );
